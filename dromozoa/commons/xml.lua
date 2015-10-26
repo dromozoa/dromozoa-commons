0404fdad469fb934521f376f15907c87d0fd0e82
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-10-26 21:02:01 +0900

    インターフェースを拡張。

--- dromozoa/commons/xml.lua
+++ dromozoa/commons/xml.lua
@@ -15,16 +15,33 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-commons.  If not, see <http://www.gnu.org/licenses/>.
 
-local escape_char = {
-  [string.char(0x26)] = "&amp;";
-  [string.char(0x3C)] = "&lt;";
-  [string.char(0x3E)] = "&gt;";
-  [string.char(0x22)] = "&quot;";
-  [string.char(0x27)] = "&apos;";
-}
+local set = require "dromozoa.commons.set"
 
-local function escape(value)
-  return (tostring(value):gsub("[&<>\"']", escape_char))
+local function get_named_char_refs()
+  return {
+    [string.char(0x26)] = "&amp;";
+    [string.char(0x3C)] = "&lt;";
+    [string.char(0x3E)] = "&gt;";
+    [string.char(0x22)] = "&quot;";
+    [string.char(0x27)] = "&apos;";
+  }
+end
+
+local function get_numeric_char_refs(i, j)
+  local references = {}
+  for i = i, j do
+    references[string.char(i)] = string.format("&#x%x;", i)
+  end
+  return references
+end
+
+local char_refs = set.set_union(get_named_char_refs(), get_numeric_char_refs(0, 127))
+
+local function escape(value, pattern)
+  if pattern == nil then
+    pattern = "[&<>\"']"
+  end
+  return (tostring(value):gsub(pattern, char_refs))
 end
 
 return {
