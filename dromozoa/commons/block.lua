4a242b2639b6bc1e86e7d75c572e70fb69aa8110
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-12-04 08:28:34 +0900

    blockを修正。

--- dromozoa/commons/block.lua
+++ dromozoa/commons/block.lua
@@ -23,9 +23,9 @@ local class = {}
 
 function class.new(n)
   local self = {
-    i = 1;
+    i = 0;
   }
-  for i = 1, n do
+  for i = 1, n * 4 do
     self[i] = 0
   end
   return self
@@ -37,7 +37,7 @@ function class:update(s, i, j)
 
   local n = #self
   local j = self.i
-  local m = min + n - j
+  local m = min + n - (j + 1)
   if max > m then
     max = m
   end
@@ -45,11 +45,11 @@ function class:update(s, i, j)
   for i = min + 3, max, 4 do
     local p = i - 3
     local a, b, c, d = s:byte(p, i)
-    self[j] = a
-    self[j + 1] = b
-    self[j + 2] = c
-    self[j + 3] = d
     j = j + 4
+    self[j - 3] = a
+    self[j - 2] = b
+    self[j - 1] = c
+    self[j] = d
   end
 
   local i = max + 1
@@ -57,22 +57,22 @@ function class:update(s, i, j)
   if p < i then
     local a, b, c = s:byte(p, max)
     if c then
-      self[j] = a
-      self[j + 1] = b
-      self[j + 2] = c
       j = j + 3
+      self[j - 2] = a
+      self[j - 1] = b
+      self[j] = c
     elseif b then
-      self[j] = a
-      self[j + 1] = b
       j = j + 2
+      self[j - 1] = a
+      self[j] = b
     else
-      self[j] = a
       j = j + 1
+      self[j] = a
     end
   end
 
-  if j > n then
-    self.i = 1
+  if j >= n then
+    self.i = 0
   else
     self.i = j
   end
@@ -81,7 +81,7 @@ function class:update(s, i, j)
 end
 
 function class:is_full()
-  return self.i == 1
+  return self.i == 0
 end
 
 function class:word(i)
