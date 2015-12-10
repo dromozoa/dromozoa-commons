133d98feaba3ec2b15d9fcb6fea6c8d05832c227
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-12-10 11:00:20 +0900

    xml_escapeを分離。

--- dromozoa/commons/xml_escape.lua
+++ dromozoa/commons/xml_escape.lua
@@ -15,24 +15,31 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-commons.  If not, see <http://www.gnu.org/licenses/>.
 
-local xml_escape = require "dromozoa.commons.xml_escape"
-local xml_parser = require "dromozoa.commons.xml_parser"
+local set = require "dromozoa.commons.set"
 
-local function parse(this)
-  return xml_parser(this):apply()
+local function get_named_char_refs()
+  return {
+    [string.char(0x26)] = "&amp;";
+    [string.char(0x3c)] = "&lt;";
+    [string.char(0x3e)] = "&gt;";
+    [string.char(0x22)] = "&quot;";
+    [string.char(0x27)] = "&apos;";
+  }
 end
 
-local class = {
-  escape = xml_escape;
-  parse = parse;
-}
-
-function class.decode(s)
-  local v, matcher = parse(s)
-  if not matcher:eof() then
-    error("cannot reach eof at position " .. matcher.position)
+local function get_numeric_char_refs(i, j)
+  local refs = {}
+  for i = i, j do
+    refs[string.char(i)] = string.format("&#x%x;", i)
   end
-  return v
+  return refs
 end
 
-return class
+local char_refs = set.set_union(get_named_char_refs(), get_numeric_char_refs(0, 127))
+
+return function (value, pattern)
+  if pattern == nil then
+    pattern = "[&<>\"']"
+  end
+  return (tostring(value):gsub(pattern, char_refs))
+end
