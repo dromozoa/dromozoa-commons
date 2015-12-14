286b52c45cd628b9c00d3e7d50b2f8e5a8b170d2
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-12-14 09:10:14 +0900

    random_bytesを追加。

---
+++ test/test_random_bytes.lua
@@ -0,0 +1,31 @@
+-- Copyright (C) 2015 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-commons.
+--
+-- dromozoa-commons is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-commons is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-commons.  If not, see <http://www.gnu.org/licenses/>.
+
+local base64 = require "dromozoa.commons.base64"
+local random_bytes = require "dromozoa.commons.random_bytes"
+
+local a = base64.encode_url(random_bytes(32))
+local b = base64.encode_url(random_bytes(32))
+local c = base64.encode_url(random_bytes(32))
+local d = base64.encode_url(random_bytes(32))
+
+assert(a ~= b)
+assert(a ~= c)
+assert(a ~= d)
+assert(b ~= c)
+assert(b ~= d)
+assert(c ~= d)
