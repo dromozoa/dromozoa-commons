ed8147aee1bcbf3caccf916de7d98cf2e06cb683
Tomoyuki Fujimori <moyu@dromozoa.com>
2016-07-18 09:06:19 +0900

    message_digestとshaを分離。

--- dromozoa/commons/sha.lua
+++ dromozoa/commons/sha.lua
@@ -1,4 +1,4 @@
--- Copyright (C) 2015 Tomoyuki Fujimori <moyu@dromozoa.com>
+-- Copyright (C) 2015,2016 Tomoyuki Fujimori <moyu@dromozoa.com>
 --
 -- This file is part of dromozoa-commons.
 --
@@ -15,16 +15,12 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-commons.  If not, see <http://www.gnu.org/licenses/>.
 
-local sequence = require "dromozoa.commons.sequence"
-local translate_range = require "dromozoa.commons.translate_range"
+local message_digest = require "dromozoa.commons.message_digest"
 local uint32 = require "dromozoa.commons.uint32"
-local uint64 = require "dromozoa.commons.uint64"
-local unpack = require "dromozoa.commons.unpack"
 
 local band = uint32.band
 local bxor = uint32.bxor
 local bnot = uint32.bnot
-local char = uint32.char
 
 local class = {
   Parity = bxor;
@@ -38,63 +34,6 @@ function class.Maj(x, y, z)
   return bxor(band(x, y), band(x, z), band(y, z))
 end
 
-function class:update(s, i, j)
-  local s = tostring(s)
-  local min, max = translate_range(#s, i, j)
-  self.L = self.L + max - min + 1
-  local M = self.M
-  while min <= max do
-    min = M:update(s, min, max)
-    if M:full() then
-      self:compute()
-    end
-  end
-  return self
-end
-
-function class:update_hmac(s, pad)
-  local s = tostring(s)
-  self.L = self.L + 64
-  local M = self.M
-  M:update(s, 1, #s)
-  M:flush()
-  M.i = 16
-  for i = 1, 16 do
-    M[i] = bxor(M[i], pad)
-  end
-  self:compute()
-  return self
-end
-
-function class:finalize(encode)
-  local M = self.M
-  M:update("\128", 1, 1)
-  if M:flush() > 14 then
-    self:compute()
-    M:reset()
-  end
-  M[15], M[16] = uint64.word(self.L * 8, ">")
-  self:compute()
-  local H = self.H
-  if encode == "hex" then
-    return self.hex_format:format(unpack(H))
-  elseif encode == "bin" then
-    local bin = sequence()
-    for i = 1, #H do
-      bin:push(char(H[i], ">"))
-    end
-    return bin:concat()
-  else
-    return H
-  end
-end
-
-function class.hmac(class, K, text, encode)
-  if #K > 64 then
-    K = class.bin(K)
-  end
-  local h = class():update_hmac(K, 0x36363636):update(text):finalize("bin")
-  return class():update_hmac(K, 0x5c5c5c5c):update(h):finalize(encode)
-end
-
-return class
+return setmetatable(class, {
+  __index = message_digest;
+})
