a8d1d1867710a52ad2f534f25f52c91bb36035c5
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-01-14 16:23:42 +0900

    cryptを追加。

---
+++ test/test_crypt_apache_sha1.lua
@@ -0,0 +1,20 @@
+-- Copyright (C) 2017 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-commons.
+--
+-- dromozoa-commons is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-commons is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-commons.  If not, see <http://www.gnu.org/licenses/>.
+
+local crypt_apache_sha1 = require "dromozoa.commons.crypt_apache_sha1"
+
+assert(crypt_apache_sha1("password") == "{SHA}W6ph5Mm5Pz8GgiULbPgzG37mj9g=")
