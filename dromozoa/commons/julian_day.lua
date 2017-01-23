1ddd201f288af9aed71a8705c5edce3e82d70137
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-01-24 08:51:28 +0900

    リファクタリング。

--- dromozoa/commons/julian_day.lua
+++ dromozoa/commons/julian_day.lua
@@ -15,9 +15,9 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-commons.  If not, see <http://www.gnu.org/licenses/>.
 
-local class = {}
-
 local floor = math.floor
+
+local class = {}
 
 function class.encode(year, month, day, hour, min, sec)
   if hour == nil then
